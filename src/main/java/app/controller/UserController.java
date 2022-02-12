9053ce45be9539f3f04d407493a17f2bea22f038
MaxWell <xaxaxa25420@gmail.com>
2022-02-12 07:01:21 +0300

    Complete

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -19,34 +19,34 @@ public class UserController {
         return "index";
     }
 
-    @GetMapping(value = "/user/add")
+    @GetMapping(value = "/add")
     public String showCreateUserPage(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "editOrCreateUser";
     }
 
-    @PostMapping(value = "/user/add")
+    @PostMapping(value = "/add")
     public String createUserPage(@ModelAttribute("user") User user) {
         userService.add(user);
         return "redirect:/";
     }
 
-    @GetMapping(value = "/user/edit")
+    @GetMapping(value = "/edit")
     public String showEditUserPage(Model model, @RequestParam(value = "userId") long userId) {
         User user = userService.getUser(userId);
         model.addAttribute("user", user);
         return "editOrCreateUser";
     }
 
-    @PostMapping(value = "/user/edit")
+    @PostMapping(value = "/edit")
     public String editUserPage(@ModelAttribute("user") User user, @RequestParam(value = "userId") long userId) {
         user.setId(userId);
         userService.changeUser(user);
         return "redirect:/";
     }
 
-    @GetMapping(value = "/user/delete")
+    @GetMapping(value = "/delete")
     public String deleteUserPage(@RequestParam(value = "userId") long userId) {
         userService.removeUser(userId);
         return "redirect:/";
