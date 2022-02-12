6231be4ad8c32d5da09845b166e469403b612f9a
MaxWell <xaxaxa25420@gmail.com>
2022-02-12 17:48:24 +0300

    Remake2

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -14,40 +14,42 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(value = "/")
+    @GetMapping()
     public String userListPage(Model model) {
         model.addAttribute("users", userService.getUsersList());
         return "index";
     }
 
-    @GetMapping(value = "/new_user")
+    @GetMapping("/new_user")
     public String showCreateUserPage(Model model) {
         User user = new User();
         model.addAttribute("user", user);
+        model.addAttribute("create", true);
         return "editOrCreateUser";
     }
 
-    @PostMapping(value = "/new_user")
+    @PostMapping("/new_user/create")
     public String createUserPage(@ModelAttribute("user") User user) {
         userService.add(user);
         return "redirect:/users/";
     }
 
-    @GetMapping(value = "/edit/{userId}")
+    @GetMapping("/{userId}/edit")
     public String showEditUserPage(Model model, @PathVariable long userId) {
         User user = userService.getUser(userId);
         model.addAttribute("user", user);
+        model.addAttribute("create", false);
         return "editOrCreateUser";
     }
 
-    @PostMapping(value = "/edit/{userId}")
+    @PostMapping("/{userId}/edit/submit")
     public String editUserPage(@ModelAttribute("user") User user, @PathVariable long userId) {
         user.setId(userId);
         userService.changeUser(user);
         return "redirect:/users/";
     }
 
-    @GetMapping(value = "/delete/{userId}")
+    @GetMapping("/{userId}/delete")
     public String deleteUserPage(@PathVariable long userId) {
         userService.removeUser(userId);
         return "redirect:/users/";
